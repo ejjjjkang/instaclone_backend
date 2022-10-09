7830d60ca917ae7d4b88d891c03c2ab740593b56
eunkang <ek646@cornell.edu>
2022-10-09 15:06:27 -0400

    user profile

--- users/users.mutation.js
+++ users/users.mutation.js
@@ -1,24 +1,51 @@
 import client from "../client";
+import bcrypt from "bcrypt";
 
 export default {
 	Mutation: {
 		createAccount: async (
 			_,
-			{ firstName, lastName, userName, email, password },
+			{ firstName, lastName, userName, email, password }
 		) => {
-			//check if username or email are already exist
-			const existingUser = await client.user.findFirst({
-				where: {
-					OR: [
-						{
-							username: userName,
-						},
-						{
-							email,
-						},
-					],
-				},
-			});
+			try {
+				//check if username or email are already exist
+				const existingUser = await client.user.findFirst({
+					where: {
+						OR: [
+							{
+								username: userName,
+							},
+							{
+								email,
+							},
+						],
+					},
+				});
+				if (existingUser) {
+					throw new Error("This username / password is already taken");
+				}
+				const uglyPassword = await bcrypt.hash(password, 10);
+				return client.user.create({
+					data: {
+						userName,
+						email,
+						firstName,
+						lastName,
+						password: uglyPassword,
+					},
+				});
+			} catch (e) {
+				return e;
+			}
+		},
+		login: async (_, { username, password }) => {
+			const user = await client.user.findFirst({ where: { username } });
+			if (!user) {
+				return {
+					ok: false,
+					error: "User not found",
+				};
+			}
 		},
 	},
 };
